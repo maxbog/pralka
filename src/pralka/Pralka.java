db8945cde629cda80d763be1fa5356c91e9d43bc
Maksymilian Boguń <mbogun@gmail.com>
2012-01-04 13:23:36 +0100

    windowsowy look and feel

--- src/pralka/Pralka.java
+++ src/pralka/Pralka.java
@@ -1,10 +1,15 @@
 package pralka;
 
+import javax.swing.UIManager;
 import pralka.ui.MainFrame;
 
 public class Pralka {
 
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+        } catch (Exception e) {
+        }
         MainFrame frame = new MainFrame();
         frame.setVisible(true);
     }
